21b0929bcf1c77923ff564b6a1286f6dbeddbeea
Ali Uygun <aliuygnnn@gmail.com>
2022-11-18 11:06:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Else_Yapısı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // www.patika.dev
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 12)
+            {
+                Console.WriteLine("GÜNAYDIN..");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İYİ GÜNLER..");
+            }
+            else {
+                Console.WriteLine("İYİ GECELER..");
+
+                
+
+
+        }
+            string sonuc = time <= 18 ? "İyi Günler!!" : "İyi Geceler!!";
+
+           string sonuc1 = time >= 6 && time < 12 ? "Günaydın*" : time <= 18 ? "İyi Günler*" : "İyi geceler*";
+            Console.WriteLine(sonuc);
+            Console.WriteLine(sonuc1);
+
+            Console.ReadLine();
+        }
+    }
+}
